6b17850ebc811ae6dbe391677680f10f7ebfc439
koSim97 <56014963+koSim97@users.noreply.github.com>
2021-11-08 14:43:51 +0900

    Create ToggleCase.java

---
+++ ToggleCase.java
@@ -0,0 +1,18 @@
+public class ToggleCase {
+    static int x=32;
+    static String toggleCase(char[] a) {
+        for (int i=0; i<a.length; i++) {
+            a[i]^=32;
+        }
+        return new String(a);
+    }
+    public static void main(String[] args) {
+        String str = "CheRrY";
+        System.out.print("Toggle case: ");
+        str = toggleCase(str.toCharArray());
+        System.out.println(str);
+        System.out.print("Original string: ");
+        str = toggleCase(str.toCharArray());
+        System.out.println(str);
+    }
+}
